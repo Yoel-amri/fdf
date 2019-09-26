0c8c193152ed4da3b519a232a834097afbc4fe33
Mohamed Bouzargtoun <mobouzar@e2r3p3.1337.ma>
2019-09-26 23:35:56 +0100

    wwww

--- main.c
+++ main.c
@@ -1,19 +1,71 @@
 #include "fdf.h"
 
-
-void    read_map(int fd)
+void	ft_free(char **tab)
 {
+	int	i;
 
+	i = -1;
+	if (**tab == NULL)
+		return ;
+	while (tab[++i] != NULL)
+	{
+		ft_strdel(&tab[i]);
+		tab[i] = NULL;
+	}
+	ft_strdel(tab);
+	tab = NULL;
+}
+
+int		**allocate(int i, int j)
+{
+	int k;
+	int **tab;
+
+	k = 0;
+	tab = (int**)malloc(sizeof(int*) * j);
+	while (k < i)
+	{
+		tab[k] = (int*)malloc(sizeof(int) * i);
+		k++;
+	}
+}
+
+int		**read_map(int fd)
+{
+	char	*line;
+	char	**tab;
+	int		count;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (get_next_line(&line, fd))
+	{
+		if (count != i)
+			return (0);
+		tab = ft_strsplit(line, ' ');
+		while (tab[i] != NULL)
+			i++;
+		count = i;
+
+		ft_free(tab);
+		j++;
+	}
+	return (allocate(i,j));
 }
 
 int     main(int argc, char **argv)
 {
     int fd;
+	int	**tab;
     if (argc != 2)
     {
         ft_putstr("ERROR\n");
         return (0);
     }
     fd = open(argv[1], O_RDWR);
-    read_map(fd);
+    tab = read_map(fd);
+	close(fd);
 }
